5eb14c286bd365d137a21ebc4656fcbba2138298
Lavínia <lalahgoliver@gmail.com>
2025-03-26 16:12:09 -0300

    Update testeMain.java

--- Exercicio1/testeMain.java
+++ Exercicio1/testeMain.java
@@ -2,8 +2,8 @@ package Exercicio1;
 
 public class testeMain {
     public static void main(String[] args) {
-        Animal Cachorro = new Cachorro();
-        Cachorro.fazerSom();
+        Cachorro cachorro = new Cachorro("Cachorro");
+        cachorro.fazerSom();
 
 
     }
